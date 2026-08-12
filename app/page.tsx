a3055a519c30827638b959279254ae07c1806947
bb_golden <itsgolden235@gmail.com>
2026-08-12 05:22:34 -0500

    fix: formatting

--- app/page.tsx
+++ app/page.tsx
@@ -20,8 +20,8 @@ export default async function Home() {
   const img_src = await getRandomImgSrc();
 
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div>
+      <main>
         <Image src={ img_src } alt = "A randomly retrieved Danbooru image." height={ 320 } width={ 320 }/>
       </main>
     </div>
